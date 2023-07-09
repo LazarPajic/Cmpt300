2239e4d2aa43515bc464976979c4fb5727dde866
LazarPajic <105390344+LazarPajic@users.noreply.github.com>
2023-07-08 20:27:12 -0700

    Add files via upload

--- myChannels.c
+++ myChannels.c
@@ -7,7 +7,7 @@
 #include <stdbool.h>
 
 #define MAX_LINE 256
-volatile atomic_flag *FLAG = ATOMIC_FLAG_INIT;
+//volatile atomic_flag *FLAG = ATOMIC_FLAG_INIT;
 
 struct metadata_struct {
 	char file_path[MAX_LINE];
@@ -20,56 +20,71 @@ struct metadata_struct {
 };
 
 void* file_calculations(void* arg){
+	//define current files struct 
 	struct metadata_struct *arg_struct2 = (struct metadata_struct*) arg;
 	
-	FILE *fp1;
-	//char line[256] = "";
-	float value = 0;
+	//define variables
+	float fvalue = 0;
 	printf("%s\n", arg_struct2->file_path);
-	fp1 = fopen(arg_struct2->file_path, "r"); 
-	//fgets(line, 256, fp1);
 	char ch;
 	char ans[arg_struct2->buffer_size];
-	int counter = 0;
+	int buff_counter = 0;
 	int pos = 0;
 	int index = 0;
 	float new_sample_value = 0;
-	bool output_complete = false;
+	//bool output_complete = false;
 	
+	//Declare file pointer and open file in reading mode
+	FILE *fp1;
+	fp1 = fopen(arg_struct2->file_path, "r"); 
+	
+	//Go through the current file character by character
 	do {
 		ch = fgetc(fp1);
 		printf("ch is: %c\n", ch);
+		
+		//add a valid character to save as a value
 		if(ch != '\n' && ch != '\r' && ch != EOF){
-			if(counter < arg_struct2->buffer_size){
+			if(buff_counter < arg_struct2->buffer_size){
 				//strcat(ans, ch);
 				ans[pos] = ch;
 				printf("***\n");
 				printf("ans is: %s\n", ans);
 				printf("***\n");
-				counter++;
+				buff_counter++;
 				pos++;
 			}
 		}
+		//treat \n as a byte for buffer_size 
 		else if(ch == '\n'){
-			counter++;
+			buff_counter++;
 		}	
 		
-		if(counter == arg_struct2->buffer_size || (ch == EOF && pos > 0)){
-			printf("counter is %d\n", counter);
+		//when length of value is equal to buffer_size or when current character equals End Of Final and there is valid value
+		//do the alpha and beta calculations
+		if(buff_counter == arg_struct2->buffer_size || (ch == EOF && pos > 0)){
+			printf("counter is %d\n", buff_counter);
 			printf("buffer is %d\n", arg_struct2->buffer_size);
-			counter = 0;
+			
+			//change to float value
+			fvalue = atof(ans);
+			printf("value is: %f\n", fvalue);
+			
+			//reset values for next line
+			buff_counter = 0;
 			pos = 0;
-			value = atof(ans);
-			printf("value is: %f\n", value);
+			
+			//save the first value to array in position zero without calculations
 			if(index == 0){
-				arg_struct2->alpha_calcs[0] = value;
+				arg_struct2->alpha_calcs[0] = fvalue;
 				printf("alpha: %f\n", arg_struct2->alpha_calcs[0]);
 				
-				arg_struct2->beta_calcs[0] = arg_struct2->beta * value;
+				arg_struct2->beta_calcs[0] = arg_struct2->beta * fvalue;
 				printf("beta: %f is %f\n", arg_struct2->beta, arg_struct2->beta_calcs[0]);
 			}
+			//do calculations and save to array
 			else{
-				new_sample_value = arg_struct2->alpha * value + (1 - arg_struct2->alpha) * arg_struct2->alpha_calcs[index - 1];	
+				new_sample_value = arg_struct2->alpha * fvalue + (1 - arg_struct2->alpha) * arg_struct2->alpha_calcs[index - 1];	
 				
 				arg_struct2->alpha_calcs[index] = new_sample_value;
 				printf("alpha: %f\n", arg_struct2->alpha_calcs[index]);
@@ -77,18 +92,23 @@ void* file_calculations(void* arg){
 				arg_struct2->beta_calcs[index] = arg_struct2->beta * new_sample_value;
 				printf("beta: %f for this %f\n", arg_struct2->beta, arg_struct2->beta_calcs[index]);
 			}
+			
+			//fill ans with terminating characters to reuse
 			for(int i = 0; i <= arg_struct2->buffer_size; i++){
 				ans[i] = '\0';
 			}
+			
+			//increase position in array
 			index++;
 		}
 		
 	} while(ch != EOF);
 	
+	//store the number of values from the file into the file's struct
 	arg_struct2->array_size = index;
 	printf("index is: %d\n", index);
-	
-
+	/*
+	//Edward's comment....
 	while(!output_complete){
 		while(!atomic_flag_test_and_set(FLAG)){
 			printf("Entering cs...\n");
@@ -97,14 +117,18 @@ void* file_calculations(void* arg){
 			output_complete = true;
 		}
 		printf("Exiting cs...\n");
-	}
-
+	}*/
+	
+	
 	pthread_exit(0);
 }
 
 
-int main(int argc, char **argv){
-	
+int main(int argc, char **argv){	
+	//define variables 
+	char line[256] = "";
+	int file_num = 0;
+	float value = 0;
 	
 	int buffer_size = atoi(argv[1]);
 	int num_threads = atoi(argv[2]);
@@ -114,16 +138,13 @@ int main(int argc, char **argv){
 	
 	pthread_t thid[num_threads];
 	
-	FILE *fp;
-	char line[256] = "";
-	int file_num = 0;
-	float value = 0;
 	printf("%s \n", metadata_path);
-	fp = fopen(metadata_path, "r"); 
 	
-	
-	// opening metadata file
+	//Declare file pointer and open file in reading mode
+	FILE *fp;
+	fp = fopen(metadata_path, "r");
 	if(fp != NULL){
+		//get the number of files
 		fgets(line, 256, fp);
 		file_num = atoi(line);		
 	}
@@ -131,47 +152,53 @@ int main(int argc, char **argv){
 		printf("something went wrong\n");
 		return 1;
 	}
-
-	// opening output file
-	FILE *fp2;
-	printf("%s \n", output_file_path);
-	fp2 = fopen(output_file_path, "w"); 
-	if(fp2 == NULL){
-		printf("Failed to open output file \n");
-		return 1;
-	}
 	
+	//Make a struct for each file and assign input values 
 	struct metadata_struct channel_files[file_num];	
 	for(int i = 0; i < file_num; i++){
+		//get the filepath and add the terminating character at the end
 		fgets(line, 256, fp);
 		int len = strcspn(line, "\n");
 		line[len] = '\0';
 		strcpy(channel_files[i].file_path, line);
 		
+		//save the alpha value
 		fgets(line, 256, fp);
 		value = atof(line);
 		channel_files[i].alpha = value;
 		
+		//save the beta value
 		fgets(line, 256, fp);
 		value = atof(line);
 		channel_files[i].beta = value;
 		
+		//save the buffer_size value
 		channel_files[i].buffer_size = buffer_size;
 	}
-		
+	
+	//Close metadata file
+	fclose(fp);
+	
+	//printing for testing purposes
 	for (int i = 0; i < file_num; i++){
 		printf("%s and %f and %f \n", channel_files[i].file_path, channel_files[i].alpha, channel_files[i].beta);
 	}
-	
-	for (int i = 0; i < num_threads; i++){
-		pthread_attr_t attr2;
-		pthread_attr_init(&attr2);
-		pthread_create(&thid[i], &attr2, file_calculations, &channel_files[i]);
+		
+	//Create threads and call file calculations
+	for(int i = 0; i < file_num/num_threads; i++){
+		for(int j = 0; j < num_threads; j++){
+			pthread_attr_t attr2;
+			pthread_attr_init(&attr2);
+			pthread_create(&thid[j], &attr2, file_calculations, &channel_files[i*num_threads + j]);
+		}
 	}
+	
+	//Wait for background threads to finish
 	for (int i = 0; i < num_threads; i++){
 		pthread_join(thid[i], NULL);
 	}
 	
+	//Calculate the results from the files and find the largest number of values
 	int results[256];
 	int largest_array_size = 0;
 	for(int k = 0; k < file_num; k++){
@@ -183,14 +210,23 @@ int main(int argc, char **argv){
 			largest_array_size = channel_files[k].array_size;
 		}
 	}
-
 	
+	//Declare file pointer and open file in writing mode
+	FILE *fp2;
+	printf("%s \n", output_file_path);
+	fp2 = fopen(output_file_path, "w"); 
+	if(fp2 == NULL){
+		printf("Failed to open output file \n");
+		return 1;
+	}
+	
+	//Write results into the output_file
 	for (int i = 0; i < largest_array_size; i++){
 		fprintf(fp2, "%d\n", results[i]);
-		//fwrite(results[i], sizeof(int), 1, fp2);
 		printf("results: %d \n", results[i]);
 	}
 	
+	//Close output_file
 	fclose(fp2);
 	
 	return 0;
